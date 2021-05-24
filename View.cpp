2c34b269dbaa1207894b64f4986f58cdfa84fd29
bannerholly <i.syuu0202@gmail.com>
2021-05-25 03:05:07 +0900

    Third Commit

--- View.cpp
+++ View.cpp
@@ -208,7 +208,7 @@ void View::specialKeyboard(int key, int x, int y){
     glutIdleFunc(idle);
     break;
   case GLUT_KEY_F5:
-    s->saveImage("XXXXXXXXXX",false);
+    s->saveImage("XXXXXXXXX",false);
   default:
     glutIdleFunc(0);
     break;
